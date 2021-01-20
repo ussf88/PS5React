d8d45b58a46af47c1321abce07efa65f34ea0c89
youssef el faquir <youssef1998islam@gmail.com>
2021-01-21 00:41:28 +0100

    $

--- src/services/userServiceOld.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const users_api="http://localhost:8080/users";
-
-class userService{
-    getUsers(){
-        return axios.get(users_api);
-    }
-}
-export default new userService();
